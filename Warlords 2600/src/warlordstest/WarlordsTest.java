60e0e015c03df21712e3dbaf4eff7730fef53b44
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2017-03-22 20:46:17 +1300

    Cleanup

--- Warlords 2600/src/warlordstest/WarlordsTest.java
+++ Warlords 2600/src/warlordstest/WarlordsTest.java
@@ -58,7 +58,6 @@ public class WarlordsTest extends TestSuite {
         assertTrue("The ball should remain within bounds", this.ball.getXPos() >= 0);
         assertTrue("The ball's velocity should be reversed in the direction of the collision", this.ball.getXVelocity() == 50 && this.ball.getYVelocity() == 50);
 
-
     }
 
     @Test
@@ -73,11 +72,6 @@ public class WarlordsTest extends TestSuite {
         this.paddle.setYPos(500);
 
         this.game.tick();
-
-        System.out.println(this.ball.getXPos());
-        System.out.println(this.ball.getYPos());
-        System.out.println(this.ball.getXVelocity());
-        System.out.println(this.ball.getYVelocity());
 
         assertTrue("The ball should not travel through the paddle", this.ball.getYPos() <= 500);
         assertTrue("The ball's velocity should be reversed in the direction of the collision", this.ball.getXVelocity() == 10 && this.ball.getYVelocity() == -10);
