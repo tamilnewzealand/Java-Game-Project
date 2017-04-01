33a6534d974bcceddb23aba3906f6d2e984cde04
Adil Bhayani <abha808@aucklanduni.ac.nz>
2017-04-01 22:12:47 +1300

    Reduced frequency of powerups

--- FreedomtoEtruaruta/src/warlords2600/Game.java
+++ FreedomtoEtruaruta/src/warlords2600/Game.java
@@ -121,7 +121,7 @@ public class Game{
                 AIs.get(i).movePaddle(ball);
             }
 
-            if (timeElapsed % 100 == 0){
+            if (timeElapsed % 900 == 0){
                 this.speedUps.add(new SpeedUp());
                 generatePowerUp(speedUps.size()-1);
                 System.out.println(speedUps.size());
