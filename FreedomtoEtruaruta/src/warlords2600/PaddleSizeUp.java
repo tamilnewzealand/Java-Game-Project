275f9f58596593a438c843010357a64723a542f3
Adil Bhayani <abha808@aucklanduni.ac.nz>
2017-04-06 19:37:17 +1200

    Both paddle sizes increase when powerup is triggered

--- FreedomtoEtruaruta/src/warlords2600/PaddleSizeUp.java
+++ FreedomtoEtruaruta/src/warlords2600/PaddleSizeUp.java
@@ -1,5 +1,7 @@
 package warlords2600;
 
+import EtruarutaGUI.Main;
+
 /**
  * Created by adilb on 3/04/2017.
  */
@@ -7,6 +9,7 @@ public class PaddleSizeUp extends PowerUp{
     public void activateEffect(Ball ball, General[] generals){
         for (int i = 0; i < generals.length; i ++){
             generals[i].paddle.checkIncreaseWidth(generals[i].paddle.getWidth()*2);
+            if (Main.numberOfPaddles == 2) generals[i].paddleFollower.checkIncreaseWidth(generals[i].paddleFollower.getWidth()*2);
         }
     }
 
